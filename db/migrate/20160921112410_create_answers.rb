450c06f773b37c09627cc33b2d9bc1f9a934d462
Hakim & Chris Johnson & Erinç  & Johnny Choo <pair+HakimJoseph.Koala-t.erinc35.jchoo157@devbootcamp.com>
2016-09-21 15:30:31 -0700

    Make corrections to answer model and question model, make corrections to answer migration.

--- db/migrate/20160921112410_create_answers.rb
+++ db/migrate/20160921112410_create_answers.rb
@@ -1,9 +1,9 @@
 class CreateAnswers < ActiveRecord::Migration
   def change
     create_table :answers do |t|
-      t.string :title, null: false
       t.string :content, null: false
       t.integer :user_id
+      t.integer :question_id
 
       t.timestamps null: false
     end

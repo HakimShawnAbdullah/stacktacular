65948632aeb64260b607b84daac7db7e57d81795
Hakim & Chris Johnson & Erinç  & Johnny Choo <pair+HakimJoseph.Koala-t.erinc35.jchoo157@devbootcamp.com>
2016-09-22 12:05:03 -0700

    allow users to add answers to questions

--- app/controllers/questions.rb
+++ app/controllers/questions.rb
@@ -2,5 +2,6 @@ get '/questions/:id' do
   @question = Question.find(params[:id])
   p "*" * 20
   @user = User.find(@question.user_id)
+  session[:question_id] = @question.id
   erb :"/questions/show"
 end

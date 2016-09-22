068ea0b56e2d5177d85409ec8e5cf2ce700a3629
Hakim & Chris Johnson & Erinç  & Johnny Choo <pair+HakimJoseph.Koala-t.erinc35.jchoo157@devbootcamp.com>
2016-09-22 09:38:30 -0700

    Create link of question that goes no where, and recent posts pulls from table.

--- app/controllers/sessions.rb
+++ app/controllers/sessions.rb
@@ -2,7 +2,7 @@ post "/sessions" do
   # get the user object
   user = User.find_by(email: params[:email])
   # authenticate the password
-  if user.password == params[:password]
+  if user && user.password == params[:password]
     # if success add something to the session
     session[:user_id] = user.id
   # redirect to index
